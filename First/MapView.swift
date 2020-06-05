92ac366cf2728fd1bcb8ca77cf069e73f3d6f9ab
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2020-06-04 22:48:49 -0700

    add MapView

---
+++ First/MapView.swift
@@ -0,0 +1,22 @@
+//
+//  MapView.swift
+//  First
+//
+//  Created by Afzal Khan on 6/4/20.
+//  Copyright © 2020 SKFJ. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
