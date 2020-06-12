7f425172708e05c682ef506e4359795b5fb88696
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2020-06-12 12:36:20 -0700

    added welcome screen files

---
+++ First/First/Welcome Screen/Extensions+Modifiers.swift
@@ -0,0 +1,40 @@
+//
+//  Extensions+Modifiers.swift
+//  First
+//
+//  Created by Afzal Khan on 6/6/20.
+//  Copyright © 2020 SKFJ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .font(.headline)
+            .padding()
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+            .background(RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .fill(Color.mainColor))
+            .padding(.bottom)
+    }
+}
+
+extension View {
+    func customButton() -> ModifiedContent<Self, ButtonModifier> {
+        return modifier(ButtonModifier())
+    }
+}
+
+extension Text {
+    func customTitleText() -> Text {
+        self
+            .fontWeight(.black)
+            .font(.system(size: 36))
+    }
+}
+
+extension Color {
+    static var mainColor = Color("Accent")
+}
